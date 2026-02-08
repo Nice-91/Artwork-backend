acd40b07e96ca6a7da6f30c329270cff27d3a35e
Nice-91 <iradukundanice91@gmail.com>
2026-02-08 09:56:53 +0200

    updating quotes and portifolio

--- backend/settings.py
+++ backend/settings.py
@@ -91,9 +91,23 @@ cloudinary.config(
 
 
 # Email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@artwork.com'
+
+# =========================
+# EMAIL CONFIGURATION
+# =========================
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'iradukundanice91@gmail.com'
+EMAIL_HOST_PASSWORD = 'kapwoqinuiniston'
+
+DEFAULT_FROM_EMAIL = 'Artwork Quotes <iradukundanice91@gmail.com>'
 CONTACT_EMAIL = 'iradukundanice91@gmail.com'
+
 
 LOGIN_REDIRECT_URL = '/admin/'
 DEBUG = True
