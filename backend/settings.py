691bbbfee48d8a52641e9f5a9383b9316c53b425
Nice-91 <iradukundanice91@gmail.com>
2026-02-08 17:50:39 +0200

    adding frontend url

--- backend/settings.py
+++ backend/settings.py
@@ -14,8 +14,10 @@ ALLOWED_HOSTS = ['artwork-backend-2.onrender.com', 'localhost', '127.0.0.1']
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
+    "https://artwork-dun.vercel.app",
     "http://localhost:5173",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 INSTALLED_APPS = [
