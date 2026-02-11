88649c46fc2d9a7da60a2c5848b1b2e7d5e8db37
Nice-91 <iradukundanice91@gmail.com>
2026-02-11 11:31:42 +0200

    adding database

--- backend/settings.py
+++ backend/settings.py
@@ -68,11 +68,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+
+if os.getenv("RENDER"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 LANGUAGE_CODE = 'en-us'
